1bfc9d43a39a88a8e749e1fd85bae5d56221d489
sdy362 <seodye04@gmail.com>
2021-10-15 11:06:10 +0900

    충돌 해결

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -4,42 +4,108 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    [SerializeField] private float speed = 10f;
+    [SerializeField] private float speed = 0f;
+    [SerializeField] private float rotateSpeed = 0f;
+    [SerializeField] private float scrollSpeed = 0f;
+
     [SerializeField] private Vector2 limitMax = Vector2.zero;
     [SerializeField] private Vector2 limitMin = Vector2.zero;
-
-    private bool isMoveMode = false;
+    [SerializeField] private float limitRotateMaxX = 0f;
+    [SerializeField] private float limitRotateMinX = 0f;
+    [SerializeField] private float limitScrollMax = 0f;
+    [SerializeField] private float limitScrollMin = 0f;
 
     private Vector3 lastMousePosition = Vector3.zero;
+    private Vector3 lastRotationMousePosition = Vector3.zero;
+
     private Vector3 direction = Vector3.zero;
     private Vector3 targetPosition = Vector3.zero;
+    private Vector3 rotateDirection = Vector3.zero;
+    private Vector3 targetRotatePosition = Vector3.zero;
+    private Vector2 scroll = Vector2.zero;
 
-    private void Update()
+    private float targetScrollValue = 0f;
+
+    private void LateUpdate()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ClickObject();
+        }
+
         if (Input.GetMouseButtonDown(1))
         {
-            isMoveMode = true;
             lastMousePosition = Input.mousePosition;
         }
 
-        if (Input.GetMouseButtonUp(1))
+        if (Input.GetMouseButton(1))
         {
-            isMoveMode = false;
+            MoveCamera();
         }
 
-        if (isMoveMode)
+        if (Input.GetMouseButtonDown(2))
         {
-            direction = lastMousePosition - Input.mousePosition;
-            direction.z = direction.y;
-            direction.y = 0;
-
-            targetPosition = transform.position + direction * speed * Time.deltaTime;
-            targetPosition.x = Mathf.Clamp(targetPosition.x, limitMin.x, limitMax.x);
-            targetPosition.z = Mathf.Clamp(targetPosition.z, limitMin.y, limitMax.y);
-
-            transform.position = targetPosition;
-
-            lastMousePosition = Input.mousePosition;
+            lastRotationMousePosition = Input.mousePosition;
         }
+
+        if (Input.GetMouseButton(2))
+        {
+            RotateCamera();
+        }
+
+        Zoom();
+    }
+
+    private void ClickObject()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hitResult))
+        {
+            Debug.Log(hitResult.transform.gameObject.name + "을 클릭했습니다.");
+        }
+    }
+
+    private void MoveCamera()
+    {
+        direction = (lastMousePosition - Input.mousePosition);
+        direction.z = direction.y;
+        direction.y = 0;
+
+        targetPosition = transform.position + direction * (speed * Camera.main.fieldOfView) * Time.deltaTime;
+        targetPosition.x = Mathf.Clamp(targetPosition.x, limitMin.x, limitMax.x);
+        targetPosition.z = Mathf.Clamp(targetPosition.z, limitMin.y, limitMax.y);
+
+        transform.position = targetPosition;
+
+        lastMousePosition = Input.mousePosition;
+    }
+
+    private void RotateCamera()
+    {
+        rotateDirection = lastRotationMousePosition - Input.mousePosition;
+
+        rotateDirection.z = rotateDirection.x;
+        rotateDirection.x = rotateDirection.y;
+        rotateDirection.y = rotateDirection.z * -1f;
+        rotateDirection.z = 0;
+
+        targetRotatePosition = transform.rotation.eulerAngles + rotateDirection * rotateSpeed * Time.deltaTime;
+
+        if (targetRotatePosition.x <= limitRotateMaxX || targetRotatePosition.x >= limitRotateMinX)
+        {
+            targetRotatePosition.z = 0f;
+            transform.rotation = Quaternion.Euler(targetRotatePosition);
+        }
+
+        lastRotationMousePosition = Input.mousePosition;
+    }
+
+    private void Zoom()
+    {
+        scroll = Input.mouseScrollDelta;
+        targetScrollValue = scroll.y * scrollSpeed * Time.deltaTime;
+
+        Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView - targetScrollValue, limitScrollMin, limitScrollMax);
     }
 }
